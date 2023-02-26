70dc8e0db755cecab9cffb5e65d255cc9aa3b313
DinislamAdin <dinislam.adin20045@gmail.com>
2023-02-26 19:16:46 +0600

    ;askdfalkds

---
+++ practice-problem.js
@@ -0,0 +1,36 @@
+// problem no-1
+
+/*const oddNumber = [1, 3, 5, 7, 9];
+
+const makeItEven = oddNumber.map(num => num + 1);
+console.log(makeItEven);
+
+
+
+// problem no-2
+
+const numbers = [34,53,41,400,14,33,45,30,50,32,15,34];
+
+const goodNumber = numbers.filter(sum => sum % 10 === 0);
+
+console.log(goodNumber);
+
+
+
+
+// problem no-3
+
+const numbers = [34,500,41,400,14,33,45,30,50,32,15,34];
+
+const goodNumber = numbers.find(sum => sum % 10 === 0);
+
+console.log(goodNumber);*/
+
+// problem
+// problem
+// problem
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
+
+  
